423349b82a78500dfd783e0d5f51c7b990555b04
martinmalinda <malindacz@gmail.com>
2019-06-18 14:39:37 +0200

    add cheer class

---
+++ cheer.js
@@ -0,0 +1,9 @@
+class Cheer {
+  static class addTeamName(teamName) {
+    const myDiv = document.createElement('h2');
+    myDiv.innerText = teamName;
+    document.body.append(myDiv);
+  }
+}
+
+Cheer.addTeamName(teamName);
